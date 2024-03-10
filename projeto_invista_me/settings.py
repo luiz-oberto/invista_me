4476710234ab5c5421b15f2edc94fa2b9bbbf46c
0b3rto <luiztaven@gmail.com>
2024-03-10 17:10:21 -0300

    prep para render

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!@2efzpdgru#uw*(-s*^b)!*r5=9i5=l%&wewg9bz_*&m!c*x@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
